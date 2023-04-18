748f7fca1cb81cd3ac410ea319f9241e2d2a26a3
Michelle Morilhas <mmorilhas@magnasistemas.com.br>
2023-04-18 08:50:40 -0300

    atualizando textos documentacao swagger

--- src/main/java/br/com/magna/confeccao/infra/config/SpringDocConfigurations.java
+++ src/main/java/br/com/magna/confeccao/infra/config/SpringDocConfigurations.java
@@ -12,14 +12,14 @@ public class SpringDocConfigurations {
 
 	@Bean
 	public OpenAPI customOpenAPI() {
-		return new OpenAPI()
-					.info(new Info().title("Confeccao API").description(
-						"API Rest da aplicação Confecção, contendo as funcionalidades de CRUD de peças de roupa superiores.\n\n"
-						+ "Suas operações permitem o mapeamento do  ")
-						.contact(new Contact().name("Desenvolvedora").email("mmorilhas@gmail.com"))
-						
-							
-							);
+		return new OpenAPI().info(new Info().title("Confeccao API").description("API Rest da aplicação Confecção.\n"
+				+ "Simplifica a comunicação entre marca e confecção: \n"
+				+ "o sistema visa facilitar a consulta feita pela confecção dos dados técnicos que são geralmente enviados no desenho técnico"
+				+ " e manter um histórico das modelagens criadas pela marca.\n\n"
+				+ "Contém as funcionalidades de CRUD de peças de roupa superiores.\n\n")
+				.contact(new Contact().name("Desenvolvedora").email("mmorilhas@gmail.com"))
+
+		);
 	}
 
 }
